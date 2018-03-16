1e630c8a6ae1f8183998f6644b63c9ee4aa4a629
kztrawz <32369635+kztrawz@users.noreply.github.com>
2018-03-16 07:25:27 -0700

    building final project

---
+++ project3/script.js
@@ -0,0 +1,17 @@
+$('video').click(function () {
+	findImage()
+});
+
+function findImage() {
+	var x = Math.floor(Math.random() * ) + 1;
+
+	switch (x) {
+		case 1:
+		changeImage();
+		break;
+
+		case 2:
+		changeImage();
+		break;
+	}
+}
