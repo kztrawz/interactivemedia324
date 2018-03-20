571e142cc928d39c0839999d656c5cc1dec2d5c1
kztrawz <32369635+kztrawz@users.noreply.github.com>
2018-03-20 15:53:47 -0700

    building final project

--- project3/script.js
+++ project3/script.js
@@ -1,49 +1,47 @@
-var images = ["water.jpg", "pool.jpg", "ocean.jpg", "waves.jpg"]
-
-
-$('video').click(function() {
-	findGallery()
-});
-
-function findGallery() {
-	var x = Math.floor(Math.random() * images.length * 4) + 1;
-
-	switch (x) {
-		case 1:
-		changeImage(findImage(images1));
-		break;
-
-		case 2:
-		changeImage(findImage(images2));
-		break;
-
-		case 3:
-		changeImage(findImage(images3));
-		break;
-
-		case 4:
-		changeImage(findImage(images4));
-		break;
-
-		default:
-		console.log("Something unexpected happened.");
-
-	}
+function random_image() {
+	var image1 = "http://kztrawz.github.io/interactivemedia324/project3/images/pond.jpg"
+	var image2 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek2.jpg"
+	var image3 = "http://kztrawz.github.io/interactivemedia324/project3/images/Flowing.jpg"
+	var image4 = "http://kztrawz.github.io/interactivemedia324/project3/images/pond2.jpg"
+	var image5 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek.jpg"
+	var image6 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek4.jpg"
+	var image7 = "http://kztrawz.github.io/interactivemedia324/project3/images/roughsea.jpg"
+	var image8 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek5.jpg"
+	var image9 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek8.jpg"
+	var image10 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek11.jpg"
+	var image11 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek9.jpg"
+	var image12 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek10.jpg"
+	var image13 = "http://kztrawz.github.io/interactivemedia324/project3/images/pool2.jpg"
+	var image14 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek12.jpg"
+	var image15 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek7.jpg"
+	var image16 = "http://kztrawz.github.io/interactivemedia324/project3/images/overwhelmed.jpg"
+	var image17 = "http://kztrawz.github.io/interactivemedia324/project3/images/bright.jpg"
+	var image18 = "http://kztrawz.github.io/interactivemedia324/project3/images/creek13.jpg"
+	var image19 = "http://kztrawz.github.io/interactivemedia324/project3/images/darkness.jpg"
+	var image20 = "http://kztrawz.github.io/interactivemedia324/project3/images/waterfall.jpg"
+	var array_of_images = new Array ();
+	console.log(array_of_images);
+	array_of_images.push(image1);
+	array_of_images.push(image2);
+	array_of_images.push(image3);
+	array_of_images.push(image4);
+	array_of_images.push(image5);
+	array_of_images.push(image6);
+	array_of_images.push(image7);
+	array_of_images.push(image8);
+	array_of_images.push(image9);
+	array_of_images.push(image10);
+	array_of_images.push(image11);
+	array_of_images.push(image12);
+	array_of_images.push(image13);
+	array_of_images.push(image14);
+	array_of_images.push(image15);
+	array_of_images.push(image16);
+	array_of_images.push(image17);
+	array_of_images.push(image18);
+	array_of_images.push(image19);
+	array_of_images.push(image20);
+	var choice = Math.floor(Math.random() * array_of_images.length);
+	document.getElementById("feelings").innerHTML = "<img src='" + array_of_images[choice] + "'></img>";
 }
 
-function changeImage(feelings) {
-	var = $("<div class=\"images\">" + feelings + "</div>");
-	rPosition(element);
-	$('#feelings').append(element);
-	element.fadeIn(500).delay(5000).fadeOut(7000, function(){
-		$(this).remove();
-	});
-}
-
-function rPosition(element){
-	var x = Math.floor(Math.random() * 65);
-	var y = Math.floor(Math.random() * 35) + 35;
-	var r = Math.floor(Math.random() * 360)
-	element.css("top", y + '%');
-	element.css("left", x + '%');
-}
